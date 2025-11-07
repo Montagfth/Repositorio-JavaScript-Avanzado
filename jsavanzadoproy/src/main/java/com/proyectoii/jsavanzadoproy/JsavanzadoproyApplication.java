5cb01196f8d0614b39cfc9f61559a6dbd2429fe2
Fabrizio <andrewmp2122@gmail.com>
2025-11-06 20:14:43 -0500

    Mods-Ins: Carga de proyecto base 2

---
+++ jsavanzadoproy/src/main/java/com/proyectoii/jsavanzadoproy/JsavanzadoproyApplication.java
@@ -0,0 +1,13 @@
+package com.proyectoii.jsavanzadoproy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsavanzadoproyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsavanzadoproyApplication.class, args);
+	}
+
+}
